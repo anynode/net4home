4ac5891becbdddd48234bb0004d420a56fdbcb9c
anynode <koerber@te-systems.com>
2025-05-16 17:41:46 +0200

    Update const.py

--- custom_components/net4home/const.py
+++ custom_components/net4home/const.py
@@ -24,14 +24,3 @@ DEFAULT_MI = 65281
 DEFAULT_OBJADR = 32700
 CONF_MI = "MI"
 CONF_OBJADR = "OBJADR"
-```python
-"""Constants for the net4home integration."""
-
-DOMAIN = "net4home"
-DEFAULT_PORT = 3478
-SERVICE_TYPE = "_n4hbuscon._tcp.local."
-DEFAULT_MI = 65281
-DEFAULT_OBJADR = 32700
-
-CONF_MI = "MI"
-CONF_OBJADR = "OBJADR"
