52ce98e640718913a9500e18bfde9a415f921d1a
anynode <koerber@te-systems.com>
2025-05-18 16:34:24 +0200

    Update binary_sensor.py

--- custom_components/net4home/binary_sensor.py
+++ custom_components/net4home/binary_sensor.py
@@ -1,24 +1,20 @@
-from homeassistant.helpers.dispatcher import async_dispatcher_connect
+from homeassistant.components.binary_sensor import BinarySensorEntity
 from homeassistant.core import callback
+from .const import DOMAIN
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    client = hass.data[DOMAIN][entry.entry_id]
+    async_add_entities([Net4HomeBinarySensor(client)], True)
 
 class Net4HomeBinarySensor(BinarySensorEntity):
-    def __init__(self, client, objadr):
+    def __init__(self, client):
         self.client = client
-        self._objadr = objadr
         self._state = False
-
-    async def async_added_to_hass(self):
-        self.async_on_remove(
-            async_dispatcher_connect(
-                self.hass, f"net4home_update_{self._objadr}", self._handle_update
-            )
-        )
-
-    @callback
-    def _handle_update(self, value):
-        self._state = bool(value)
-        self.async_write_ha_state()
 
     @property
     def is_on(self) -> bool:
         return self._state
+
+    async def async_update(self) -> None:
+        # TODO: fetch and update sensor state
+        pass
