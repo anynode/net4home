396daf71e8e1ae0694d85c9803a46eb731be30fe
anynode <koerber@te-systems.com>
2025-05-19 16:40:27 +0200

    Update api.py

--- custom_components/net4home/api.py
+++ custom_components/net4home/api.py
@@ -65,7 +65,7 @@ class Net4HomeApi:
         self._logger.debug(f"Gesendetes Paket mit Länge (hex): {packet_with_len[:4].hex()}")
         self._logger.debug(f"Gesendetes Paket komplett (hex): {packet_with_len.hex()}")
 
-        """self._writer.write(packet_with_len)"""
+        # self._writer.write(packet_with_len)
         
         self._writer.write("190000000002ac0f400a000002bc02404600000487000000c000000200") 
         await self._writer.drain()
